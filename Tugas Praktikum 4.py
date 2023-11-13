c9794a36a90c2667b86deeb6f89c736fc0f76f1f
Muhammad Fiqri Firmansyah <mfiqrifirmansyah.92@gmail.com>
2023-11-13 20:05:30 +0700

    Praktikum4

---
+++ Tugas Praktikum 4.py
@@ -0,0 +1,42 @@
+# Inisialisasi list untuk menyimpan data mahasiswa
+data_mahasiswa = []
+
+
+# Fungsi untuk menghitung nilai akhir
+def hitung_nilai_akhir(tugas, uts, uas):
+    nilai_akhir = (tugas * 0.3) + (uts * 0.35) + (uas * 0.35)
+    return nilai_akhir
+
+
+# Perulangan untuk memasukkan data
+while True:
+    # Meminta input data mahasiswa
+    nama = input("Masukkan nama mahasiswa: ")
+    tugas = float(input("Masukkan nilai tugas: "))
+    uts = float(input("Masukkan nilai UTS: "))
+    uas = float(input("Masukkan nilai UAS: "))
+
+    # Menghitung nilai akhir
+    nilai_akhir = hitung_nilai_akhir(tugas, uts, uas)
+
+    # Menambahkan data ke dalam list
+    data_mahasiswa.append({
+        'Nama': nama,
+        'Tugas': tugas,
+        'UTS': uts,
+        'UAS': uas,
+        'Nilai Akhir': nilai_akhir
+    })
+
+    # Menanyakan apakah ingin menambahkan data lagi
+    tambah_data = input("Tambah data lagi? (y/t): ")
+    if tambah_data.lower() != 'y':
+        break
+
+# Menampilkan daftar data mahasiswa
+print("\nDaftar Data Mahasiswa:")
+print("===================================")
+print("No | Nama Mahasiswa | Nilai Akhir")
+print("===================================")
+for i, mahasiswa in enumerate(data_mahasiswa, start=1):
+    print(f"{i}  | {mahasiswa['Nama']} | {mahasiswa['Nilai Akhir']:.2f}")
